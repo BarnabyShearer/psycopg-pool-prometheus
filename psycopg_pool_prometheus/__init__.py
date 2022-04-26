65af9cf2442b05d43c2fc0d0771958ae3f14765e
Barnaby Shearer <b@zi.is>
2022-04-26 09:08:14 +0100

    Use upstreams new types (#7)

--- psycopg_pool_prometheus/__init__.py
+++ psycopg_pool_prometheus/__init__.py
@@ -5,18 +5,19 @@ from __future__ import annotations
 from typing import Iterator
 
 from prometheus_client.core import REGISTRY, GaugeMetricFamily
+from prometheus_client.registry import Collector
 from psycopg_pool import AsyncConnectionPool, ConnectionPool
 
 
 def register(pool: ConnectionPool | AsyncConnectionPool) -> None:
     """Register a posycopg_pool.Pool with prometheus_client."""
 
-    class _PoolCollector:
+    class _PoolCollector(Collector):
         def collect(self) -> Iterator[GaugeMetricFamily]:
             """Expose psycopg Pool metrics to Prometheus."""
             for key, value in pool.get_stats().items():
-                yield GaugeMetricFamily(  # type: ignore
-                    f"psycopg_pool_{key}", None, value=value
+                yield GaugeMetricFamily(
+                    f"psycopg_pool_{key}", "See psycopg Pool metrics", value=value
                 )
 
-    REGISTRY.register(_PoolCollector())  # type: ignore
+    REGISTRY.register(_PoolCollector())
